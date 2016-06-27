6c2c8dbd8b7cec3995af73eef6c20b53cf56a321
thanecoder <thanecoder@users.noreply.github.com>
2016-06-27 20:44:18 +0530

    Update Mremove.php

--- Mremove.php
+++ Mremove.php
@@ -39,55 +39,63 @@ function link1()
 @session_start();
 include_once("Head.php");
 echo "<br><br><br>";
-$id=$_SESSION['user_id'];
-$servername="localhost";
-$username="root";
-$password="";
-$sid=$_GET['sid'];
-$conn=mysqli_connect($servername,$username,$password,'moviewebsite');
-if(!$conn)
+if(isset($_SESSION['user_status']))
 {
-die ("Connection failed:".mysqli_connect_error());
-}
-$sql="delete from s_cart where s_id='$sid' and user_id='$id'";
-if(mysqli_query($conn,$sql))
-{
-$sql="select * from s_cart where user_id='$id'";
-$result=mysqli_query($conn,$sql);
-$count=0;
-$rowcount=mysqli_num_rows($result);
-for($count=0;$count<$rowcount;$count++)
-{
-	$row=mysqli_fetch_array($result);
-	echo '<div style="margin-top:50px;width:1000;height:100px">
-	<table><tr><td>NAME:</td><td>'
-	.$row['p_name'].
-	'</td></tr>
-	<tr><td>COST:</td><td>'
-	.$row['price'].
-	'</td></tr><tr><td>QUANTITY:</td><td>'
-	.$row['quantity'].
-	'</td></tr></div></table>';
-	echo '<a id="remove" href="Mremove.php?sid='.$row['s_id'].'">Remove from cart</form></a>';
-	$row++;
-}
-echo "<br><B>TOTAL BILL</B><br>";
-include_once('Mbill.php');
-$sql="select * from s_cart where user_id='$id'";
-$result=mysqli_query($conn,$sql);
-$rowcount=mysqli_num_rows($result);
-if($rowcount==0)
-{
-	echo "<br><button id='payment' onclick='link1()'>Catalogue</button>";
+	$id=$_SESSION['user_id'];
+	$servername="localhost";
+	$username="root";
+	$password="";
+	$sid=$_GET['sid'];
+	$conn=mysqli_connect($servername,$username,$password,'moviewebsite');
+	if(!$conn)
+	{
+	die ("Connection failed:".mysqli_connect_error());
+	}
+	$sql="delete from s_cart where s_id='$sid' and user_id='$id'";
+	if(mysqli_query($conn,$sql))
+	{
+		$sql="select * from s_cart where user_id='$id'";
+		$result=mysqli_query($conn,$sql);
+		$count=0;
+		$rowcount=mysqli_num_rows($result);
+		echo "<h2> YOUR CART </h2>";
+		for($count=0;$count<$rowcount;$count++)
+		{
+			$row=mysqli_fetch_array($result);
+			echo '<div style="margin-top:50px;width:1000;height:100px">
+			<table><tr><td>NAME:</td><td>'
+			.$row['p_name'].
+			'</td></tr>
+			<tr><td>COST:</td><td>'
+			.$row['price'].
+			'</td></tr><tr><td>QUANTITY:</td><td>'
+			.$row['quantity'].
+			'</td></tr></div></table>';
+			echo '<a id="remove" href="Mremove.php?sid='.$row['s_id'].'">Remove from cart</form></a>';
+			$row++;
+		}
+		echo "<br><B>TOTAL BILL</B><br>";
+		include_once('Mbill.php');
+		$sql="select * from s_cart where user_id='$id'";
+		$result=mysqli_query($conn,$sql);
+		$rowcount=mysqli_num_rows($result);
+		if($rowcount==0)
+		{
+			echo "<br><button id='payment' onclick='link1()'>Catalogue</button>";
+		}
+		else
+		{
+			echo "<br><button id='payment' onclick='link()'>Proceed to Payment</button>";
+		}
+	}
+	else
+	{
+		die(mysqli_error($conn));
+	}
 }
 else
 {
-	echo "<br><button id='payment' onclick='link()'>Proceed to Payment</button>";
-}
-}
-else
-{
-	die(mysqli_error($conn));
+	echo "<br><br><a id='login' href='Mlogin.php'>Please click here to login</a>";
 }
 ?>
 </html>
